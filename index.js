a2bc66f00ad06e2d2f7355fac99d7556ab27c1f5
johnny-mathias <johnnydiasmathiasjunior@gmail.com>
2025-08-06 22:27:40 -0300

    primeira aula

---
+++ index.js
@@ -0,0 +1,13 @@
+var nome = "johnny";
+var idade = 18;
+var ehMaiorDeIdade = true;
+var frutas = ['uva', 'abacaxi', 'maçã'];
+var indefinido = undefined;
+var nullo = null;
+var pessoa = {
+    nome: "johnny",
+    idade: 18,
+    profissao: "estudante"
+};
+pessoa.idade = 20;
+console.log(pessoa.idade);
---
+++ index.ts
@@ -0,0 +1,24 @@
+let nome = "johnny"
+
+let idade = 18
+
+let ehMaiorDeIdade = true
+
+let frutas: string[] = ['uva', 'abacaxi', 'maçã']
+
+let indefinido = undefined
+
+let nullo = null
+
+interface Pessoa {
+    nome: string
+    idade: number
+    profissao: string
+}
+
+const pessoa: Pessoa = {
+    nome:"johnny",
+    idade:18,
+    profissao:"estudante"
+}
+
